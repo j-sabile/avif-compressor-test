1efcd646c472f02a016034288adfffcf3b0b2255
j-sabile <jericosabile@gmail.com>
2024-02-08 22:00:02 +0800

    ui improvements

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -9,13 +9,11 @@
       method: "POST",
       body: formData,
     });
-    console.log(await res.json());
+    await res.json();
+    alert("Success!");
   };
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
+  const handleDragOver = (e) => e.preventDefault();
   const handleDrop = (e) => {
     e.preventDefault();
 
@@ -31,29 +29,27 @@
   };
 </script>
 
-<main>
+<main class="flex flex-col justify-center items-center my-auto min-h-[100dvh]">
   <div class="drop-area" on:dragover={handleDragOver} on:drop={handleDrop} role="application">Drag images here...</div>
-  <button on:click={handleUpload}>Upload</button>
-  <h3>Files</h3>
-  <ul>
+  <button on:click={handleUpload} class="bg-neutral-900 rounded-lg shadow px-5 py-2 m-4 hover:brightness-90">Upload</button>
+  <h3 class="text-center text-lg font-semibold my-4">Images</h3>
+  <ul class="flex flex-col gap-2">
     {#each images as image}
-      <li>{image.name}</li>
+      <li class="bg-neutral-900 rounded shadow px-4 py-1">{image.name}</li>
     {:else}
-      <li>No images added</li>
+      <p class="italic font-thin my-4">No images...</p>
     {/each}
   </ul>
 </main>
 
 <style>
   .drop-area {
-    margin: 1rem;
     border: 2px dashed gray;
     border-radius: 0.5rem;
     padding: 2rem;
     text-align: center;
-    cursor: pointer;
-    height: 200px;
-    width: 200px;
+    height: 250px;
+    width: 250px;
     display: flex;
     justify-content: center;
     align-items: center;
