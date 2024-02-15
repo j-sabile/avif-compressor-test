50cb9de40e3e85083a7a5a46afda376a3513a07c
j-sabile <jericosabile@gmail.com>
2024-02-16 00:03:26 +0800

    not working

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,18 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import sharp from "sharp";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const ORIGIN = "http://localhost:5173";
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors({ origin: ORIGIN }));
 
 const upload = multer();
-
 app.get("/", (req, res) => res.send("HELLO WORLD"));
 
 app.post("/image", upload.array("img"), async (req, res) => {
@@ -17,6 +21,7 @@ app.post("/image", upload.array("img"), async (req, res) => {
   const quality = parseInt(req.body.quality);
   const effort = parseInt(req.body.effort);
   const resolution = parseInt(req.body.resolution);
+  const socketId = req.body.socketId;
 
   const promises = images.map(async (img) => {
     await sharp(img.buffer)
@@ -25,11 +30,29 @@ app.post("/image", upload.array("img"), async (req, res) => {
       .keepExif()
       .keepIccProfile()
       .toFile(`../Compressed Images/${img.originalname.substring(0, img.originalname.lastIndexOf("."))}.avif`);
+    io.to(socketId).emit("compressed", img.originalname);
   });
 
   await Promise.all(promises);
   res.status(200).json({ message: "Success" });
 });
 
+app.post("/download", (req, res) => {});
+
+// SOCKET.IO
+const users = new Set();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: ORIGIN } });
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
+  users.add(String(socket.id));
+
+  socket.on("disconnect", () => {
+    users.delete(String(socket.id));
+    console.log(`${socket.id} disconnected`);
+  });
+});
+
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
+server.listen(PORT, () => console.log(`Listening to port ${PORT}`));
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { io } from "socket.io-client";
+
   import Slider from "./components/Slider.svelte";
   import SingleCompress from "./components/SingleCompress.svelte";
   let images = [];
@@ -7,6 +9,13 @@
   let resolution = "1080";
   let isCompressing = false;
   let isSingleCompress = false;
+  let connected = false;
+  let canDownload = true;
+
+  const socket = io(import.meta.env.VITE_API);
+  socket.on("compressed", (t) => console.log(t + " received"));
+
+  socket.on("connect", () => (connected = true));
 
   const handleCompress = async () => {
     isCompressing = true;
@@ -14,13 +23,14 @@
     formData.append("quality", quality);
     formData.append("effort", effort);
     formData.append("resolution", resolution);
+    formData.append("socketId", socket.id);
     images.forEach((img) => formData.append("img", img));
 
     const res = await fetch(`${import.meta.env.VITE_API}/image`, {
       method: "POST",
       body: formData,
     });
-    await res.json();
+    if (res.status == 200) canDownload = true;
     alert("Success!");
     isCompressing = false;
   };
@@ -48,43 +58,47 @@
     <main class="flex flex-row flex-wrap justify-center gap-10 my-auto">
       <!-- UPLOAD SECTION -->
       <section class="flex flex-col justify-center items-center gap-6 max-w-[250px] max-h-[300px] my-auto">
-        <div
-          class="drop-area w-full h-[100px] sm:h-[200px]"
-          on:dragover={handleDragOver}
-          on:drop={handleDrop}
-          role="application"
-        >
+        <div class="drop-area w-full h-[100px] sm:h-[200px]" on:dragover={handleDragOver} on:drop={handleDrop} role="application">
           Drag images here...
         </div>
         <div class="flex flex-col justify-center items-center gap-2">
           <button
             on:click={handleCompress}
-            class={`bg-neutral-900 rounded-lg shadow px-5 py-2 ${isCompressing ? "brightness-75" : "hover:brightness-90"}`}
-            disabled={isCompressing}>{isCompressing ? `Compressing...` : "Compress"}</button
+            class={`bg-neutral-900 rounded-lg shadow px-5 py-2 ${
+              images.length === 0 || isCompressing || !connected ? "brightness-75" : "hover:brightness-90"
+            }`}
+            disabled={images.length === 0 || isCompressing || !connected}>{isCompressing ? `Compressing...` : "Compress"}</button
           >
           <button
             on:click={() => (isSingleCompress = true)}
-            class={`bg-neutral-900 rounded-lg shadow px-5 py-2 hover:brightness-90 ${
-              images.length === 0 ? "brightness-75" : "hover:brightness-90"
-            }`}
+            class={`bg-neutral-900 rounded-lg shadow px-5 py-2 ${images.length === 0 ? "brightness-75" : "hover:brightness-90"}`}
             disabled={images.length === 0}>Single Compress</button
           >
         </div>
       </section>
 
       <!-- IMAGES LIST SECTION -->
-      <section class="flex flex-col gap-2 justify-center items-center h-full w-[300px] max-h-[250px]">
-        <h3 class="text-center text-lg font-semibold">Images</h3>
-        <ul class="flex flex-col gap-2 w-full overflow-auto scroll pe-2">
-          {#each images as image, index}
-            <li class="flex flex-row justify-between bg-neutral-900 rounded-md shadow w-full px-4 py-1">
-              <p>{image.name}</p>
-              <button on:click={() => (images = images.filter((_, i) => i !== index))}>&#x2715;</button>
-            </li>
-          {:else}
-            <p class="italic font-thin text-center my-4">No images...</p>
-          {/each}
-        </ul>
+      <section class="flex flex-col gap-2 justify-between items-center h-full w-[300px] max-h-[250px]">
+        <div class="flex flex-col gap-2 justify-center items-center w-full">
+          <h3 class="text-center text-lg font-semibold">Images</h3>
+          <ul class="flex flex-col gap-2 w-full overflow-auto scroll pe-2">
+            {#each images as image, index}
+              <li class="flex flex-row justify-between bg-neutral-900 rounded-md shadow w-full px-4 py-1">
+                <p>{image.name}</p>
+                <button on:click={() => (images = images.filter((_, i) => i !== index))}>&#x2715;</button>
+              </li>
+            {:else}
+              <p class="italic font-thin text-center my-4">No images...</p>
+            {/each}
+          </ul>
+        </div>
+        {#if canDownload}
+          <button
+            on:click={() => (isSingleCompress = true)}
+            class={`bg-neutral-900 rounded-lg shadow px-5 py-2 ${images.length === 0 ? "brightness-75" : "hover:brightness-90"}`}
+            disabled={images.length === 0}>Download</button
+          >
+        {/if}
       </section>
 
       <!-- SETTINGS SECTION -->
@@ -92,7 +106,7 @@
         <Slider title="Quality" min="1" max="100" bind:value={quality} />
         <Slider title="Effort" min="0" max="9" bind:value={effort} />
         <Slider title="Resolution" min="144" max="3456" bind:value={resolution} />
-      </section>  
+      </section>
     </main>
   </div>
 {/if}
